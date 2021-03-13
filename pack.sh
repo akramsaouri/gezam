38d40755699aaa51c40b8b72e2f1afaabb60732b
akramsaouri <ak.saouri@gmail.com>
2021-03-13 18:32:47 +0100

    Update app name

--- pack.sh
+++ pack.sh
@@ -1,6 +1,6 @@
 export PATH=$PATH:$(go env GOPATH)/bin 
 export GOPATH=$(go env GOPATH)
 go build         
-appify -name gezam -icon icons/1024.png gezam
+appify -name Gezam -icon icons/1024.png gezam
 chmod +x gezam.app/Contents/MacOS/gezam.app
 rm gezam 
